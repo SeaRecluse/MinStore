e982e7f7358dddbd373f3b34250f979d60778e62
SeaRecluse <503410551@qq.com>
2019-05-31 17:24:04 +0800

    Add files via upload

--- 玩具/offlineEnglishTTS/TestForUser.py
+++ 玩具/offlineEnglishTTS/TestForUser.py
@@ -3,12 +3,17 @@ import time
 
 saveChoice = True
 
-res = {'data': 'Hello world, , , , , ,Hello world'}	
+main.voiceID = 0 #huihui
+main.init()
+
+res = {'data': '帅帅帅'}	
 start = time.time()	
 main.saveTTS(res["data"])
 print("cost : " + str(time.time() - start) + " s")
 
-res = {'data': 'hahaha'}
-start = time.time()
-main.saveTTS(res["data"],saveChoice)
-print("cost : " + str(time.time() - start) + " s")
+# main.voiceID = 1
+# main.init()
+# res = {'data': 'hahaha'}
+# start = time.time()
+# main.saveTTS(res["data"],saveChoice)
+# print("cost : " + str(time.time() - start) + " s")
--- 玩具/offlineEnglishTTS/main.py
+++ 玩具/offlineEnglishTTS/main.py
@@ -3,6 +3,7 @@ import engine
 import time
 import sapi5
 
+voiceID = 1
 savePath = "./saveTTS/"
 
 _activeEngines = weakref.WeakValueDictionary()
@@ -12,21 +13,23 @@ except KeyError:
     eng = engine.Engine()
     _activeEngines[None] = eng
 
-""" RATE"""
-rate = eng.getProperty('rate')   # getting details of current speaking rate
-# print (rate)                        #printing current voice rate
-eng.setProperty('rate', 150)     # setting up new voice rate
+def init():
+
+	""" RATE"""
+	rate = eng.getProperty('rate')   # getting details of current speaking rate
+	# print (rate)                        #printing current voice rate
+	eng.setProperty('rate', 150)     # setting up new voice rate
 
 
-"""VOLUME"""
-volume = eng.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-# print (volume)                          #printing current volume level
-eng.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+	"""VOLUME"""
+	volume = eng.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+	# print (volume)                          #printing current volume level
+	eng.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
-"""VOICE"""
-voices = eng.getProperty('voices')       #getting details of current voice
-#eng.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-eng.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+	"""VOICE"""
+	voices = eng.getProperty('voices')       #getting details of current voice
+	#eng.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+	eng.setProperty('voice', voices[voiceID].id)   #changing index, changes voices. 1 for female
 
 
 def saveTTS(outA,saveWav = False):
